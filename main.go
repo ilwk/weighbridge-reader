b1262efeb6ef67a84f0522a7e0fc34c9d33dd097
luwj <luwj.dev@gmail.com>
2025-05-26 14:35:08 +0800

    refactor(main): 移除systray并使用模板渲染HTML

--- main.go
+++ main.go
@@ -4,12 +4,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"text/template"
 	"weightbridge-reader/cmd/reader"
 	"weightbridge-reader/internal/config"
 	"weightbridge-reader/internal/ws"
-
-	"fyne.io/systray"
-	"fyne.io/systray/example/icon"
 )
 
 func main() {
@@ -29,37 +27,28 @@ func main() {
 		}
 	}()
 
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl, err := template.ParseFiles("./web/static/index.html")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var data = struct {
+			WebsocketPort int
+		}{
+			WebsocketPort: cfg.WebsocketPort,
+		}
+		err = tmpl.Execute(w, data)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		ws.HandleWebSocket(w, r, dataChan)
 	})
 
 	addr := fmt.Sprintf(":%d", cfg.WebsocketPort)
 	fmt.Printf("地磅读取服务已启动，运行在 http://localhost%s/ws\n", addr)
-	http.ListenAndServe(addr, nil)
-
-	systray.Run(onReady, onExit)
-}
-
-func onReady() {
-	systray.SetIcon(icon.Data)
-	systray.SetTitle("地磅读取服务")
-	systray.SetTooltip("地磅读取服务")
-	mQuit := systray.AddMenuItem("退出", "退出程序")
-	mOpen := systray.AddMenuItem("打开页面", "打开程序")
-	go func() {
-		for {
-			select {
-			case <-mQuit.ClickedCh:
-				systray.Quit()
-			case <-mOpen.ClickedCh:
-				fmt.Println("打开页面")
-				return
-			}
-		}
-	}()
-
-}
-
-func onExit() {
-	// TODO
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
