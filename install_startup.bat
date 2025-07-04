ff3bb38817aa7d114188bf7722d7d1d406122bd1
luwj <luwj.dev@gmail.com>
2025-07-04 15:11:20 +0800

    feat: 更新开机启动脚本

--- install_startup.bat
+++ install_startup.bat
@@ -1,5 +1,5 @@
 @echo off
 set EXE_PATH=%~dp0reader.exe
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v WeighbridgeWS /d "%EXE_PATH%" /f
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v WeighbridgeWS /d "%EXE_PATH%" /f
 echo 开机启动项已添加: %EXE_PATH%
 pause
